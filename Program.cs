a602c7f644fd909dc3d58e8b8bf61ddfce5ffda9
BrodieJNR <Brodiej_Newman@hotmail.com>
2026-01-26 14:46:24 -0700

    program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using Mission4AssignmentGroup4_8;
+using System;
+using System.Linq.Expressions;
+using System.Security.Principal;
+
+
+class Program
+{
+
+    static void Main()
+    {
+
+    
+        Console.WriteLine("Welcome to Tic Tac Toe!");
+        Console.WriteLine("Players will take turns choosing the spots using 0-8!");
+
+        string[] board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+        int currentPlayer = 1;
+        string win = "";
+
+        while (win == "")
+        {
+            Console.Clear();
+            Supporting.PrintBoard(board);
+
+            string symbol = currentPlayer == 1 ? "X" : "O";
+
+            Console.WriteLine($"PLayer {currentPlayer} choose a position 0-8: ");
+            string choice = Console.ReadLine();
+
+            if (!int.TryParse(choice, out int position) || position < 0 || position > 8)
+            {
+                Console.WriteLine("Not an option please choose another spot");
+                Console.ReadLine();
+                continue;
+            }
+
+            if (board[position] == "X" || board[position] == "O")
+            {
+                Console.WriteLine("That spot is taken. Press Enter then choose again.");
+                Console.ReadLine();
+                continue;
+            }
+
+            board[position] = symbol;
+
+            win = Supporting.CheckWinner(board);
+
+            if (win == "")
+            {
+                currentPlayer = currentPlayer == 1 ? 2 : 1;
+            }
+        }
+
+        Console.Clear();
+        Supporting.PrintBoard(board);
+
+        if (win == "Draw")
+        {
+            Console.WriteLine("It's a draw!");
+        }
+        else
+        {
+            
+            int winnerNumber = win == "X" ? 1 : 2;
+            Console.WriteLine($"Player {winnerNumber} ({win}) wins!");
+        }
+
+        Console.WriteLine("Thanks for playing!");
+        Console.ReadLine();
+    }
+}
+    
+    
+
+
